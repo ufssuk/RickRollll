1c275b87c1b025d335aa9c105b710dbbab481549
Uģis Krūkliņš <138369039+ufssuk@users.noreply.github.com>
2023-08-16 17:26:01 +0300

    Kodi bez stiliem

---
+++ script.js
@@ -0,0 +1,11 @@
+const coolButton = document.querySelector('.js-cool')
+const dontButton = document.querySelector('.js-dont')
+
+coolButton.addEventListener('mouseover',() =>{
+    window.open('https://www.youtube.com/watch?v=Kog8aL8D3LM')
+})
+
+dontButton.addEventListener('mouseover',() =>{
+    window.open('https://www.youtube.com/watch?v=bv9bf5Ar_WI')
+
+})
